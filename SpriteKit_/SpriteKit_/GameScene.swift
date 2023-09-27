3b7074b6f0479bfe0d8e79644c0e5887d3645e53
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-09-27 04:05:59 +0100

    updated

--- SpriteKit_/SpriteKit_/GameScene.swift
+++ SpriteKit_/SpriteKit_/GameScene.swift
@@ -11,9 +11,16 @@ class GameScene: SKScene {
     
     var backGroundImage = SKSpriteNode(imageNamed: "background")
     
-    var nodeIcon = SKSpriteNode(imageNamed: "snake")
+    var nodeIcon: SKSpriteNode?
     
-    override func didMove(to view: SKView) {
+    var node = SKSpriteNode(imageNamed: "")
+    
+    var walkingFrames = SKTexture(imageNamed: "snake")
+    
+   public override func didMove(to view: SKView) {
+        
+       nodeIcon = SKSpriteNode(texture: walkingFrames)
+        
        //MARK: - Positioning the background image
         backGroundImage.position = CGPoint(x: 0, y: 0)
         backGroundImage.size.width = self.size.width
@@ -25,14 +32,44 @@ class GameScene: SKScene {
         
         
         //MARK: - Positioning the icon
-        nodeIcon.zPosition = 1
-        nodeIcon.color = SKColor.black
-        nodeIcon.colorBlendFactor = 1.0
-        nodeIcon.position = CGPoint(x: size.width * 0.0, y: size.height * 0.1)
+       nodeIcon?.zPosition = 1
+       nodeIcon?.color = SKColor.black
+        nodeIcon?.colorBlendFactor = 1.0
+        nodeIcon?.position = CGPoint(x: size.width * 0.0, y: size.height * 0.1)
         let iconSize = CGSize(width: 50, height: 50)
-        nodeIcon.size = iconSize
-        addChild(nodeIcon)
+        nodeIcon?.size = iconSize
+       addChild(nodeIcon ?? node)
+        
+       //MARK: - Animate the icon
+        animateSnake()
     }
     
+    func animateSnake() {
+        let animateFrames = [walkingFrames]
+        let walkingAnimation = SKAction.animate(with: animateFrames, timePerFrame: 0.1)
+        let repeatAction = SKAction.repeatForever(walkingAnimation)
+        nodeIcon?.run(repeatAction, withKey: "walking")
+        nodeIcon?.removeAction(forKey: "walking")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            nodeIcon?.position.x = (location.x)
+            nodeIcon?.position.y = (location.y)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            nodeIcon?.position.x = (location.x)
+            nodeIcon?.position.y = (location.y)
+        }
+    }
+    
+//    override func update(_ currentTime: TimeInterval) {
+//        super.update(currentTime)
+//    }
     
 }
