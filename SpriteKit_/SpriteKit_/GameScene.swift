60b587fe3dae129e6bdb4e93be5d4ae7755c4be0
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-09-29 04:31:18 +0100

    updated

--- SpriteKit_/SpriteKit_/GameScene.swift
+++ SpriteKit_/SpriteKit_/GameScene.swift
@@ -11,17 +11,18 @@ class GameScene: SKScene {
     
     var backGroundImage = SKSpriteNode(imageNamed: "background")
     
-    var iconNode: SKSpriteNode?
+    var snakeNode = SKSpriteNode(imageNamed: "snakeIcon")
     
-    var node = SKSpriteNode(imageNamed: "")
-    
-    var walkingFrames = SKTexture(imageNamed: "snakeIcon")
+    var antsNodeArray = ["antsIcon1", "antsIcon2", "antsIcon3", "antsIcon4", "antsIcon5", "antsIcon6", "antsIcon7", "antsIcon8", "antsIcon9", "antsIcon10"]
     
     public override func didMove(to view: SKView) {
-        
-        iconNode = SKSpriteNode(texture: walkingFrames)
-        
-        //MARK: - Positioning the background image
+        settingBackgroundImage()
+        createSnakeNode()
+        createAntsNode()
+    }
+    
+    //MARK: - Positioning the background image
+    func settingBackgroundImage() {
         backGroundImage.position = CGPoint(x: 0, y: 0)
         backGroundImage.size.width = self.size.width
         backGroundImage.size.height = self.size.height
@@ -29,33 +30,50 @@ class GameScene: SKScene {
         addChild(backGroundImage)
         backGroundImage.scene?.scaleMode = .aspectFill
         backGroundImage.zPosition = -1
-        
-        
-        //MARK: - Positioning the icon
-        iconNode?.zPosition = 1
-        iconNode?.color = SKColor.black
-        iconNode?.colorBlendFactor = 1.0
-        iconNode?.position = CGPoint(x: size.width * 0.0, y: size.height * 0.1)
-        let iconSize = CGSize(width: 50, height: 50)
-        iconNode?.size = iconSize
-        addChild(iconNode ?? node)
-        
-        //MARK: - Animate the icon
     }
     
+    //MARK: - Positioning the icon
+    func createSnakeNode() {
+        snakeNode.zPosition = 1
+        snakeNode.color = SKColor.black
+        snakeNode.colorBlendFactor = 1.0
+        snakeNode.position = CGPoint(x: -120, y: size.height * 0.1)
+        let iconSize = CGSize(width: 90, height: 90)
+        snakeNode.size = iconSize
+        addChild(snakeNode)
+    }
+    
+    //MARK: - Positioning the icon
+    func createAntsNode() {
+        for i in antsNodeArray {
+            let antsNode = SKSpriteNode(imageNamed: i)
+            antsNode.zPosition = 1
+            antsNode.color = SKColor.black
+            antsNode.colorBlendFactor = 1.0
+            antsNode.position = CGPoint(x: CGFloat.random(in: 0..<200), y: CGFloat.random(in: 0..<300))
+            let iconSize = CGSize(width: 30, height: 30)
+            antsNode.size = iconSize
+            addChild(antsNode)
+        }
+    }
+
+    //MARK: - Animate the icon
+    
+    //MARK: - The Method that trigger the movement of the icon
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            iconNode?.position.x = (location.x)
-            iconNode?.position.y = (location.y)
+            snakeNode.position.x = (location.x)
+            snakeNode.position.y = (location.y)
         }
     }
     
+    //MARK: - The Method that trigger the movement of the icon
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            iconNode?.position.x = (location.x)
-            iconNode?.position.y = (location.y)
+            snakeNode.position.x = (location.x)
+            snakeNode.position.y = (location.y)
         }
     }
     
